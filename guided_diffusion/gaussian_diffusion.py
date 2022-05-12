ed1aa1c4b3174b5e3557540e4de0e806b93ff1ee
kostarion <kostarion@gmail.com>
2022-05-12 15:08:54 +0000

    Add symmetry enforce function

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -851,6 +851,8 @@ class GaussianDiffusion:
         init_image=None,
         randomize_class=False,
         cond_fn_with_grad=False,
+        transformation_fn=None,
+        transformation_percent=[]
     ):
         """
         Use DDIM to sample from the model and yield intermediate samples from
@@ -870,6 +872,7 @@ class GaussianDiffusion:
             init_image = th.zeros_like(img)
 
         indices = list(range(self.num_timesteps - skip_timesteps))[::-1]
+        transformation_steps = [int(len(indices)*i) for i in transformation_percent]
 
         if init_image is not None:
             my_t = th.ones([shape[0]], device=device, dtype=th.long) * indices[0]
@@ -888,6 +891,8 @@ class GaussianDiffusion:
                                                size=model_kwargs['y'].shape,
                                                device=model_kwargs['y'].device)
             with th.no_grad():
+                if i in transformation_steps and transformation_fn is not None:
+                  img = transformation_fn(img)
                 sample_fn = self.ddim_sample_with_grad if cond_fn_with_grad else self.ddim_sample
                 out = sample_fn(
                     model,
